c491bf4a9c28d6e2750e8bdf409b693160997767
hbh <578092215@qq.com>
2023-05-16 18:07:15 +0800

    feat: 使用fastify作为底层框架，使用版本控制

--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,19 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { createViteServer } from './vite-server';
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   const viteServer = await createViteServer();
   app.use(viteServer.middlewares);
   await app.listen(3000);
